741a632e17b66806896315fd7d194007897ed3e6
KrasnosheevMikhail <mkrasn@yandex.com>
2025-09-03 15:56:11 +0300

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,71 @@
+import java.util.Objects;
+import java.util.OptionalInt;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected String city;
+    protected int age;
+
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public boolean hasAge() {
+        return age >= 0;
+    }
+
+    public boolean hasAddress() {
+        return city != null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+        return OptionalInt.of(age);
+    }
+
+    public String getAddress() {
+        return city;
+    }
+
+    public void setAddress(String address) {
+        this.city = address;
+    }
+
+    public void happyBirthday() {
+        if (hasAge()) age++;
+    }
+
+    @Override
+    public String toString() {
+        return "Person {" + "name: " + name + ", surname: " + surname
+                + ", age: " + age + ", adress: " + city + "}";
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder()
+                .setSurname(surname)
+                .setAddress(city);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getSurname(), getAge(), getAddress());
+    }
+
+}
