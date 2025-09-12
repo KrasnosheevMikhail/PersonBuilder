0200ff1cf2f9c912ab273b107cbb796ef2144045
KrasnosheevMikhail <mkrasn@yandex.com>
2025-09-12 21:17:37 +0300

    Add files via upload

--- src/Person.java
+++ src/Person.java
@@ -36,7 +36,7 @@ public class Person {
     }
 
     public OptionalInt getAge() {
-        return age;
+        return hasAge() ? age : OptionalInt.empty();
     }
 
     public String getAddress() {
@@ -56,7 +56,7 @@ public class Person {
     @Override
     public String toString() {
         return "Person {" + "name: " + name + ", surname: " + surname
-                + ", age: " + (hasAge() ? age.getAsInt() : "возраст неизвестен") +
+                + ", age: " + (hasAge() ? age.getAsInt() : "возраст не указан") +
                 ", address: " + (hasAddress() ? city : "адресс не указан") + "}";
     }
 
